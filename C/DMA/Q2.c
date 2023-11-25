8ad7452c3f1970e3333ea3e5e040de639ae96c8a
S.Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2023-11-25 13:07:53 +0000

    Create Q2.c

---
+++ C/DMA/Q2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main ()
+{
+	int n;
+	printf("enter size of an array\n");
+	scanf("%d",&n);
+	int *a=(int*)calloc(n,sizeof(int));
+		for (int i=0;i<n;i++)
+	{
+		printf("%d ",a[i]);
+	}
+return 1;
+}// end main
