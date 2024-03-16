aebd12046a1f5945e021e4f6137207f43e6ce049
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-03-16 08:03:31 +0000

    Update Q6.cpp

--- C++/OOP inher/Q6.cpp
+++ C++/OOP inher/Q6.cpp
@@ -17,12 +17,24 @@ class base{
         //cout<<"Duration of class: "<<classDuration<<endl;
       //}
 };
-class derived : public base
+class basee{
+    public:
+
+      string Name1;
+      int rollNo3;
+      basee(string Name1,int rollNo3)
+      {
+        this->Name1=Name1;
+        this->rollNo3=rollNo3;
+      }
+
+};
+class derived : public base, public basee
 {
    string name1;
    int rollNo1;
    public:
-    derived(string name1,int rollNo1, string Name, int rollNo):base (Name,rollNo)
+    derived(string name1,int rollNo1, string Name, int rollNo,string Name1,int rollNo3):base (Name,rollNo),basee(Name1,rollNo3)
       {
         this->name1=name1;
         this->rollNo1=rollNo1;
@@ -35,10 +47,13 @@ class derived : public base
         cout<<"data of drived class\n";
         cout<<"Name of student: "<<name1<<endl;
         cout<<"Roll number of student: "<<rollNo1<<endl;
+        cout<<"data of basee class\n";
+        cout<<"Name of student: "<<Name1<<endl;
+        cout<<"Roll number of student: "<<rollNo3<<endl;
       }
 };
 int main()
 {
-    derived d1("Ali",23,"asad",45);
+    derived d1("Ali",23,"Sara",33,"john",78);
     d1.display();
 }
