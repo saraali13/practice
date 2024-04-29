5d26f410d37af118fc4cece5e1975d6d278d1173
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-04-29 23:43:22 +0500

    Create Q9.cpp

---
+++ C++/OOP polym/Q9.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+// Generic class using a template: class used for all data types
+
+template<typename T>
+class Pair {
+private:
+    T first;
+    T second;
+public:
+    Pair(T f, T s) : first(f), second(s) {}
+T sum()
+{
+	return first+second;
+}
+void display (){
+	cout<<"first: "<<first<<endl;
+	cout<<"second: "<<second<<endl;
+}
+};
+
+int main() {
+    // Creating objects of Pair class with different data types
+    Pair<int> intobj(5, 10);
+    Pair<double> doubleobj(3.590, 96.8);
+    Pair<char> charobj('a', 'z');
+
+int a=intobj.sum();
+cout<<a<<endl;
+intobj.display();
+
+double b=doubleobj.sum();
+cout<<b<<endl;
+doubleobj.display();
+
+char c=charobj.sum();
+cout<<c<<endl;
+charobj.display();
+
+    return 0;
+}
