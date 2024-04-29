6089869cc919b7db7f8ad188cec31f4a41b1716e
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-04-29 21:52:17 +0500

    Update Q1.cpp

--- C++/OOP polym/Q1.cpp
+++ C++/OOP polym/Q1.cpp
@@ -1,1 +1,36 @@
-ass
+#include <iostream>
+using namespace std;
+
+//using simple virtual funtions
+
+class base//base class
+{
+	int x;
+	public:
+		virtual int sq(int x)//virtul function
+	{
+		return x*x;
+	}
+};
+
+class derived1:public base//class derived from base class
+{
+	int x;
+	public:
+		int sq(int x)//funtion over ridded
+		{
+			x=x+3;
+			return x*x;
+		}
+	
+};
+int main()
+{
+	base B1;
+	derived1 D1;
+	base* Bptr = & D1;//base pointer to derived class obj
+	int a=Bptr->sq(2);//function calling in derived class through base class pointer
+	cout<<"a= "<<a;
+	a=B1.sq(2);//funtion calling in base class
+	cout<<"\na= "<<a;
+}
