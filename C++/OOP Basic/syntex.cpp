eb1a8073efbce117c8f92adf1416ad4b46a00cf4
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-05-08 23:53:40 +0500

    Update syntex.cpp

--- C++/OOP Basic/syntex.cpp
+++ C++/OOP Basic/syntex.cpp
@@ -7,23 +7,35 @@ int *char=new int[size];
 
 template <typename T>
 template <typename T, typename U>//for 2 diff data types
+
 class <int>obj1;//template in class generic class
+//inheritance among genric classes
+template <typename T>
+class Cat : public Animal<T>
+
 void car::getdata(){}//coutside class function defination
+
 clss(const clss &obj)//copy constructor
+
 float Account :: rateOfInterest = 6.5;//definig a static variable outside the class
+
 //overridig 
 A *a;
 B b;
 a = &b;
 a->show();
+
 virtual void draw()//virtual funct
+
 friend void dothis(class c1)//frnd function dec in class
 friend class F;//frnd class dec in a class
+
 //op overloading
 class operator +(const class &C1){
   class t;
 t.xyz=xyz+c1.xyz;
 }
+
 //exp handling
 try {
   if blablabla
