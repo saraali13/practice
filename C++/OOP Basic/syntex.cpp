d3be187887de635695969f28eae8ff85db4ea8e8
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-05-08 23:40:18 +0500

    Create syntex.cpp

---
+++ C++/OOP Basic/syntex.cpp
@@ -0,0 +1,31 @@
+int * ptr= new int;
+int *char=new int[size];
+
+template <typename T>
+template <typename T, typename U>//for 2 diff data types
+class <int>obj1;//template in class generic class
+void car::getdata(){}//coutside class function defination
+clss(const clss &obj)//copy constructor
+float Account :: rateOfInterest = 6.5;//definig a static variable outside the class
+//overridig 
+A *a;
+B b;
+a = &b;
+a->show();
+virtual void draw()//virtual funct
+friend void dothis(class c1)//frnd function dec in class
+friend class F;//frnd class dec in a class
+//op overloading
+class operator +(const class &C1){
+  class t;
+t.xyz=xyz+c1.xyz;
+}
+//exp handling
+try {
+  if blablabla
+throw 1;
+}
+  catch (int a)(...)//for bht sari conditions {
+    cout<<a;
+  }
+
