c8d76980c59cb87d1d5542e8a453b218293ef46e
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-05-19 00:09:07 +0500

    Update Q8.cpp

--- C++/Exception handling/Q8.cpp
+++ C++/Exception handling/Q8.cpp
@@ -8,16 +8,22 @@ int main()
     cout << "Enter two integers (a and b): ";
     cin >> a >> b;
 
-    try {
-        if (b == 0) {
-            throw runtime_error("cannot be divided by 0");
-        } else {
+    try
+    {
+        if (b == 0) 
+        {
+            throw runtime_error("cannot be divided by 0");//if we'll not write the type of error then it will not give desired output 
+        } 
+        else 
+        {
             n = a / b;
         }
-    } catch (const exception &e) {
+    } 
+    catch (const exception &e) 
+        {
         cout << "Exception: " << e.what() << endl;
-        return 1;  // Exit the program with an error code
-    }
+        return 1;  // Exit the program with an error code else it will show some garbage value after the exception's output
+        }
 
     cout << "Result: " << n << endl;
     return 0;
