5582337696726a6ed3788e5fd9da2b3ac8e729a5
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-09-24 01:24:28 +0500

    Update Q7.py

--- Python/Pandas/Q7.py
+++ Python/Pandas/Q7.py
@@ -1,5 +1,8 @@
 import panda from pd
-d1={"a":["a1","a2","a3"],"b":["b1","b2","b3"]}
-d2={"c":["c1","c2","c3"],"d":["d1","d2","d3"]}
-a=pd.merge(d1,d2)
+
+
+d1=pd.DataFrame({"a":["a1","a2","a3"],"b":["b1","b2","b3"]})
+d2=pd.DataFrame({"c":["a1","a2","a3"],"d":["d1","b2","d3"]})
+
+a=pd.merge(d1,d2) #same waly srf
 
