120a554cd4678850d75a2be82baa5ef434c3a6ae
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-09-23 23:27:04 +0500

    Create Q1.py

---
+++ Python/prac/filing/Q1.py
@@ -0,0 +1,9 @@
+file = open("C:/Users/Fast/Desktop/new_file1.txt","w")
+
+L = ["This is Lagos \n","This is Python \n","This is Fcc \n"]
+
+file.write("Hello There \n")
+
+file.writelines(L)
+
+file.close()
