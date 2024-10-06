d0cabb131e12ec45df466312e450e3a53f4e3766
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-10-07 00:02:22 +0500

    Create Q3.py

---
+++ Python/Numpy/Q3.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+arr1=[1,2,3,4]
+arr2=[5,7,9,1]
+arr1+arr2
+arr1-arr2
+arr1*arr2
+arr1.sum()
+arr1.max()
+arr1.mean()
+arr=np.array([[2,3,4],[4,6,7],[1,9,8]])
+arr.sum(axis=0)#col wise 
+arr.sum(axis=1)#row wise
+arr.shape()
