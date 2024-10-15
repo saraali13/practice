9f69a90e6faa824c753ece08bec55e72d306e292
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-10-15 15:58:45 +0500

    Update Q1.py

--- Python/prac/Dict/Q1.py
+++ Python/prac/Dict/Q1.py
@@ -6,7 +6,20 @@ keys = ['name', 'age', 'city']
 values = ['Alice', 25, 'New York']
 my_dict = dict(zip(keys, values)
 
+list_of_lists = [['name', 'Bob'], ['age', 30], ['city', 'San Francisco']]
+my_dict = dict(list_of_lists)
+
 nested_dict = {
     'person1': {'name': 'Alice', 'age': 25},
     'person2': {'name': 'Bob', 'age': 30}
 } 
+
+my_dict = {
+    'fruits': ['apple', 'banana', 'cherry'],
+    'vegetables': ['carrot', 'lettuce', 'spinach']
+}
+my_dict = {
+    'fruits': ('apple', 'banana', 'cherry'),
+    'vegetables': ('carrot', 'lettuce', 'spinach')
+}
+
