75e94b421495c5edefaa0c6f34d44db3d041212d
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-10-15 15:53:13 +0500

    Create Q1.py

---
+++ Python/prac/List/Q1.py
@@ -0,0 +1,9 @@
+my_list = list('hello')
+print(my_list)
+my_list = ['a'] * 3 #['a','a','a']
+my_list = list(range(10))
+
+string = "apple banana cherry"
+my_list = string.split()
+print(my_list)
+nested_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] 
