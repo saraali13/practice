bdebc7f36459cb51bbb9750533b1538664f0abc6
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-10-15 15:55:45 +0500

    Create Q1.py

---
+++ Python/prac/Dict/Q1.py
@@ -0,0 +1,12 @@
+my_dict = {'name': 'Alice', 'age': 25, 'city': 'New York'}
+my_dict = dict(name='Alice', age=25, city='New York')
+my_dict = dict([('name', 'Alice'), ('age', 25), ('city', 'New York')])
+
+keys = ['name', 'age', 'city']
+values = ['Alice', 25, 'New York']
+my_dict = dict(zip(keys, values)
+
+nested_dict = {
+    'person1': {'name': 'Alice', 'age': 25},
+    'person2': {'name': 'Bob', 'age': 30}
+} 
