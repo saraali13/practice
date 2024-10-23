76b78ed9d58d4f00e89bb3a9d367a8e6fa363ca2
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-10-23 09:21:37 +0500

    Update Q2.py

--- Python/Data vis/Q2.py
+++ Python/Data vis/Q2.py
@@ -1,10 +1,9 @@
 from matplotlib import pyplot as plt
 import pandas as pd
 
-df = pd.read_csv("new.csv")
+df = pd.read_csv("Iris.csv")
 x = df["Species"]
-y = df["PetalWidthCm"]
-color = ['green', 'purple', 'yellow', 'blue','red']
-plt.scatter(x, y, c=color , marker="*")
-
+y = df["SepalLengthCm"]
+clr = ['g', 'm', 'y', 'b']
+plt.bar(x, y, color=clr)
 plt.show()
