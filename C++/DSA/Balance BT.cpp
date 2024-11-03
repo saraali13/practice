416510ddb44a90032af8f909767a2041daad522c
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-11-03 18:16:38 +0500

    Update Balance BT.cpp

--- C++/DSA/Balance BT.cpp
+++ C++/DSA/Balance BT.cpp
@@ -1,3 +1,8 @@
+ int height(Node* node) {
+        if (!node) return 0;
+        return max(height(node->left), height(node->right)) + 1;
+    }
+
 bool isBalanced(Node* node) {
         if (!node) return true;
         int leftHeight = height(node->left);
