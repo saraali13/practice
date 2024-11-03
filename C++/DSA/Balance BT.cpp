bff6aa42230d9254ab3ed497c52d8b95796ed753
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-11-03 18:14:58 +0500

    Create Balance BT.cpp

---
+++ C++/DSA/Balance BT.cpp
@@ -0,0 +1,6 @@
+bool isBalanced(Node* node) {
+        if (!node) return true;
+        int leftHeight = height(node->left);
+        int rightHeight = height(node->right);
+        return abs(leftHeight - rightHeight) <= 1 && isBalanced(node->left) && isBalanced(node->right);
+    }
