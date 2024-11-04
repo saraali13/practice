b5f911cb82346563a3cdca80a769a7bc8ec21b7c
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-11-05 00:37:35 +0500

    Create Q4.py

---
+++ Python/NLP/Q4.py
@@ -0,0 +1,20 @@
+import nltk
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+
+nltk.download("wordnet")
+stemmer = PorterStemmer()
+lemmatizer = WordNetLemmatizer()
+
+word_list = ["Jump", "Jumping", "Jumped", "Run", "Running", "Easily", "Tasty"]
+
+# Apply stemming to each word in the list
+st_words = [stemmer.stem(word) for word in word_list]
+print(f"Original word list: {word_list}\nStemmed Word list: {st_words}\n")
+
+# Apply lemmatization (converting words to lowercase as it works effectively on lower cases)
+lem_words = [lemmatizer.lemmatize(word.lower()) for word in word_list]
+print(f"Lemmatized Word list: {lem_words}")
+
+lem_words_verb = [lemmatizer.lemmatize(word.lower(), pos='v') for word in word_list]
+print(f"Lemmatized verb Word list: {lem_words_verb}")
