69065dbf183be70177b23e71f5807f01d846ff9e
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-11-04 20:53:40 +0500

    Update Q1.py

--- Python/Diamond Problem/Q1.py
+++ Python/Diamond Problem/Q1.py
@@ -1,1 +1,20 @@
+class A:
+  def display(self):
+    print("Hi A")
 
+class B(A):
+  def display(self):
+    print("Hi "B)
+
+class C(A):
+  def display(self):
+    print("Hi C")
+
+class D(B,C):
+  def display(self):
+    print("Hi D")
+
+d1=D()
+d1.display()
+
+# first will check for D class if not found then B then C and then A
