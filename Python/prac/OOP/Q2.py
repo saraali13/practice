df05410553e2130497143b3a3bd6d23c2440a18a
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2024-12-03 18:12:56 +0500

    Create Q2.py

---
+++ Python/prac/OOP/Q2.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print(f"Hi, I'm {self.name}")
+
+p = Person("Alice", 25)
+p.greet()
