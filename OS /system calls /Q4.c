50ed00e2f46eca34cecae73587a07325da257bba
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-03-11 16:02:36 +0500

    Create Q4.c

---
+++ OS /system calls /Q4.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Program is starting...\n");
+    exit(0);  // Program ends immediately here
+    printf("This will not be printed.\n");  // Never executed
+}
