f78c83c41a3d057cddd42e7b9cff1df2c67b33c2
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-03-11 15:58:59 +0500

    Update Q1.c

--- OS /system calls /Q1.c
+++ OS /system calls /Q1.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+int main() {
+    pid_t pid;
+    pid = fork(); // Creating a new process
+
+    if (pid < 0) {
+        printf("Fork failed!\n");
+        exit(1);
+    }
+    else if (pid == 0) {
+        printf("Child process, PID: %d\n", getpid());
+    }
+    else {
+        printf("Parent process, PID: %d\n", getpid());
+    }
+    return 0;
+}
