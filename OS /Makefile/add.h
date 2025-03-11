fb6e8247bb08cbeb8702dde0f5a551a37e474e61
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-03-12 00:27:42 +0500

    Create add.h

---
+++ OS /Makefile/add.h
@@ -0,0 +1,7 @@
+#ifndef ADD_H
+#define ADD_H
+
+int add(int a, int b);
+
+#endif
+//for class make class and declare its func and constructor incase if other class var are used then add their .h file
