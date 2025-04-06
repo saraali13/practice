55f7e7d50200e3499107bebff9627a60c380ab15
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-04-07 00:24:53 +0500

    Create Q6.c

---
+++ OS /Threads /Q6.c
@@ -0,0 +1,29 @@
+#include <pthread.h>
+#include <stdio.h>
+
+int counter = 0;
+pthread_mutex_t lock;
+
+void* increment(void* arg) {
+    for (int i = 0; i < 1000000; i++) {
+        pthread_mutex_lock(&lock);   // Entry section
+        counter++;
+        pthread_mutex_unlock(&lock); // Exit section
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t t1, t2;
+    pthread_mutex_init(&lock, NULL);
+
+    pthread_create(&t1, NULL, increment, NULL);
+    pthread_create(&t2, NULL, increment, NULL);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    pthread_mutex_destroy(&lock);
+    printf("Counter = %d\n", counter);
+    return 0;
+}
