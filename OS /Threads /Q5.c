9c6277e08ae657d3f7db6fc73e69bd1f498c6ca1
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-04-07 00:23:08 +0500

    Create Q5.c

---
+++ OS /Threads /Q5.c
@@ -0,0 +1,23 @@
+#define NUM_THREADS 5
+
+void *print_thread_id(void *arg) {
+    int id = *((int *) arg);
+    printf("Thread ID: %d\n", id);
+    return NULL;
+}
+
+int main() {
+    pthread_t threads[NUM_THREADS];
+    int ids[NUM_THREADS];
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        ids[i] = i;
+        pthread_create(&threads[i], NULL, print_thread_id, &ids[i]);
+    }
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    return 0;
+}
