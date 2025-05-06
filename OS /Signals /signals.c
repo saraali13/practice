440959c620800e031dd5cd20cee61b25d7064a25
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-05-06 19:21:52 +0500

    Update signals.c

--- OS /Signals /signals.c
+++ OS /Signals /signals.c
@@ -3,3 +3,10 @@ SIGTERM //Graceful termination
 SIGKILL //Forced Kill (cannot be caught)
 SIGCHLD //Child terminated
 SIGALRM //Timer alarm
+
+signal(int signum/*Signal type*/, sighandler_t handler/*Handler function or ignore*/);
+
+struct sigaction sa;
+sa.sa_handler = my_handler;
+sigaction(SIGINT, &sa, NULL);
+
