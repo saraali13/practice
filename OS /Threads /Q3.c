155441e33dd3a8fe325536f555aeaa76bd5247aa
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-05-06 19:04:46 +0500

    Update Q3.c

--- OS /Threads /Q3.c
+++ OS /Threads /Q3.c
@@ -1,26 +1,42 @@
-#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
-int counter = 0;
-pthread_mutex_t lock;
+#define SIZE 8             // Total elements in array
+#define NUM_THREADS 4      // Number of threads
 
-void* increment(void* arg) {
-    for (int i = 0; i < 1000000; i++) {
-        pthread_mutex_lock(&lock);
-        counter++;
-        pthread_mutex_unlock(&lock);
+int arr[SIZE] = {1, 2, 3, 4, 5, 6, 7, 8};
+int partial_sum[NUM_THREADS] = {0};
+
+void* sum_array(void* arg) {
+    int thread_part = *(int*)arg;
+    int start = thread_part * (SIZE / NUM_THREADS);
+    int end = start + (SIZE / NUM_THREADS);
+
+    for (int i = start; i < end; i++) {
+        partial_sum[thread_part] += arr[i];
     }
-    return NULL;
+
+    pthread_exit(0);
 }
 
 int main() {
-    pthread_t t1, t2;
-    pthread_mutex_init(&lock, NULL);
-    pthread_create(&t1, NULL, increment, NULL);
-    pthread_create(&t2, NULL, increment, NULL);
-    pthread_join(t1, NULL);
-    pthread_join(t2, NULL);
-    pthread_mutex_destroy(&lock);
-    printf("Counter: %d\n", counter);
+    pthread_t threads[NUM_THREADS];
+    int thread_ids[NUM_THREADS];
+    int total_sum = 0;
+
+    // Create threads
+    for (int i = 0; i < NUM_THREADS; i++) {
+        thread_ids[i] = i;
+        pthread_create(&threads[i], NULL, sum_array, &thread_ids[i]);
+    }
+
+    // Wait for threads to complete
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+        total_sum += partial_sum[i];
+    }
+
+    printf("Total Sum = %d\n", total_sum);
     return 0;
 }
