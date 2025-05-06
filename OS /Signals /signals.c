b479cf2d164c84a662997987204bd91aea093c90
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-05-06 19:26:15 +0500

    Update signals.c

--- OS /Signals /signals.c
+++ OS /Signals /signals.c
@@ -1,9 +1,11 @@
 SIGINT //ctrl C
 SIGTERM //Graceful termination
+SIGSEGV//segmentation fault
+SEGUSR1,SEGUSR2 //User defined
 SIGKILL //Forced Kill (cannot be caught)
 SIGCHLD //Child terminated
 SIGALRM //Timer alarm
-
+SIG_IGN //Ignore the signal
 signal(int signum/*Signal type*/, sighandler_t handler/*Handler function or ignore*/);
 
 struct sigaction sa;
