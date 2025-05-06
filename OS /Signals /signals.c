d5003286d35dbc6bf1821006149d190582f19a35
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-05-06 23:36:18 +0500

    Update signals.c

--- OS /Signals /signals.c
+++ OS /Signals /signals.c
@@ -5,10 +5,30 @@ SEGUSR1,SEGUSR2 //User defined
 SIGKILL //Forced Kill (cannot be caught)
 SIGCHLD //Child terminated
 SIGALRM //Timer alarm
+SIGILL //illegal instruction
+SIGQUIT //Quit from keyboard ctrl \
+SIGPIPE //wrting to a pipe with no read end
+SIGTERM //process termination
+SIGCONT //resume execution if stopped
+SIGSTOP //stop process execution , ctrl Z
+SIGSTP //stopped process issued from tty
+SIGTTIN/TTOUT //background process requies input / output
+SIGURG //urgent condition on socket
+SIGXCPU //CPU time limit exceeded
+SIGXFSZ //File size limit exceeded
+
 SIG_IGN //Ignore the signal
+SIG_ERR //error in signal creation
 signal(int signum/*Signal type*/, sighandler_t handler/*Handler function or ignore*/);
 
 struct sigaction sa;
 sa.sa_handler = my_handler;
-sigaction(SIGINT, &sa, NULL);
+sigemptyset(&sa.sa_mask);
+sa.sa_falgs=0;
+sigaction(SIGINT, &sa, NULL); // -1 == error
 
+while(1)
+{
+  // perform task or wait for the signal to call the signal funtion 
+}
+return EXIT_SUCCESS;
