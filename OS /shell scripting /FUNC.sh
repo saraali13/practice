e661a3257ddeebe235830f1c9498832958b67ac1
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-05-06 19:16:45 +0500

    Update FUNC.sh

--- OS /shell scripting /FUNC.sh
+++ OS /shell scripting /FUNC.sh
@@ -3,3 +3,33 @@ greet() {
     echo "Hello, $1!"
 }
 greet "Sara"   # Output: Hello, Sara!
+
+#multiple var
+info() {
+    echo "Name: $1"
+    echo "Age: $2"
+    echo "City: $3"
+}
+
+info "Ali" 22 "Karachi"
+
+
+#variable passing
+
+greet() {
+    name=$1
+    echo "Hello, $name!"
+}
+
+user="Alice"
+greet "$user"
+
+
+#Func with Return Value
+add() {
+    sum=$(( $1 + $2 ))
+    echo $sum
+}
+
+result=$(add 5 7)
+echo "Result is $result"
