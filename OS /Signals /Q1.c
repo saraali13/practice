52254f72fe5319f951e5eb6dfac2db8b1a200ab7
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-05-06 19:06:46 +0500

    Update Q1.c

--- OS /Signals /Q1.c
+++ OS /Signals /Q1.c
@@ -1,1 +1,12 @@
+#include <signal.h>
+#include <stdio.h>
+
+void handler(int sig) {
+    printf("Caught signal %d\n", sig); // ctrl C
+}
+
+int main() {
+    signal(SIGINT, handler);
+    while (1);
+}
 
